bed8ac3a8efae08aaf40e73185eaaed589ab0429
Peter Heywood <peethwd@gmail.com>
2015-01-21 13:23:49 +0000

    half cube dimensions so it has an edge length of 1 unit

--- sdl_exp/Cube.cpp
+++ sdl_exp/Cube.cpp
@@ -17,48 +17,48 @@ void Cube::render(){
 
 
 			// Begin drawing the color cube with 6 quads
-			// Top face (y = 1.0f)
+			// Top face (y = 0.5f)
 			// Define vertices in counter-clockwise (CCW) order with normal pointing out
-			glColor3f(0.0f, 1.0f, 0.0f);     // Green
-			glVertex3f(1.0f, 1.0f, -1.0f);
-			glVertex3f(-1.0f, 1.0f, -1.0f);
-			glVertex3f(-1.0f, 1.0f, 1.0f);
-			glVertex3f(1.0f, 1.0f, 1.0f);
+			glColor3f(0.0f, 0.5f, 0.0f);     // Green
+			glVertex3f(0.5f, 0.5f, -0.5f);
+			glVertex3f(-0.5f, 0.5f, -0.5f);
+			glVertex3f(-0.5f, 0.5f, 0.5f);
+			glVertex3f(0.5f, 0.5f, 0.5f);
 
-			// Bottom face (y = -1.0f)
-			glColor3f(1.0f, 0.5f, 0.0f);     // Orange
-			glVertex3f(1.0f, -1.0f, 1.0f);
-			glVertex3f(-1.0f, -1.0f, 1.0f);
-			glVertex3f(-1.0f, -1.0f, -1.0f);
-			glVertex3f(1.0f, -1.0f, -1.0f);
+			// Bottom face (y = -0.5f)
+			glColor3f(0.5f, 0.5f, 0.0f);     // Orange
+			glVertex3f(0.5f, -0.5f, 0.5f);
+			glVertex3f(-0.5f, -0.5f, 0.5f);
+			glVertex3f(-0.5f, -0.5f, -0.5f);
+			glVertex3f(0.5f, -0.5f, -0.5f);
 
-			// Front face  (z = 1.0f)
-			glColor3f(1.0f, 0.0f, 0.0f);     // Red
-			glVertex3f(1.0f, 1.0f, 1.0f);
-			glVertex3f(-1.0f, 1.0f, 1.0f);
-			glVertex3f(-1.0f, -1.0f, 1.0f);
-			glVertex3f(1.0f, -1.0f, 1.0f);
+			// Front face  (z = 0.5f)
+			glColor3f(0.5f, 0.0f, 0.0f);     // Red
+			glVertex3f(0.5f, 0.5f, 0.5f);
+			glVertex3f(-0.5f, 0.5f, 0.5f);
+			glVertex3f(-0.5f, -0.5f, 0.5f);
+			glVertex3f(0.5f, -0.5f, 0.5f);
 
-			// Back face (z = -1.0f)
-			glColor3f(1.0f, 1.0f, 0.0f);     // Yellow
-			glVertex3f(1.0f, -1.0f, -1.0f);
-			glVertex3f(-1.0f, -1.0f, -1.0f);
-			glVertex3f(-1.0f, 1.0f, -1.0f);
-			glVertex3f(1.0f, 1.0f, -1.0f);
+			// Back face (z = -0.5f)
+			glColor3f(0.5f, 0.5f, 0.0f);     // Yellow
+			glVertex3f(0.5f, -0.5f, -0.5f);
+			glVertex3f(-0.5f, -0.5f, -0.5f);
+			glVertex3f(-0.5f, 0.5f, -0.5f);
+			glVertex3f(0.5f, 0.5f, -0.5f);
 
-			// Left face (x = -1.0f)
-			glColor3f(0.0f, 0.0f, 1.0f);     // Blue
-			glVertex3f(-1.0f, 1.0f, 1.0f);
-			glVertex3f(-1.0f, 1.0f, -1.0f);
-			glVertex3f(-1.0f, -1.0f, -1.0f);
-			glVertex3f(-1.0f, -1.0f, 1.0f);
+			// Left face (x = -0.5f)
+			glColor3f(0.0f, 0.0f, 0.5f);     // Blue
+			glVertex3f(-0.5f, 0.5f, 0.5f);
+			glVertex3f(-0.5f, 0.5f, -0.5f);
+			glVertex3f(-0.5f, -0.5f, -0.5f);
+			glVertex3f(-0.5f, -0.5f, 0.5f);
 
-			// Right face (x = 1.0f)
-			glColor3f(1.0f, 0.0f, 1.0f);     // Magenta
-			glVertex3f(1.0f, 1.0f, -1.0f);
-			glVertex3f(1.0f, 1.0f, 1.0f);
-			glVertex3f(1.0f, -1.0f, 1.0f);
-			glVertex3f(1.0f, -1.0f, -1.0f);
+			// Right face (x = 0.5f)
+			glColor3f(0.5f, 0.0f, 0.5f);     // Magenta
+			glVertex3f(0.5f, 0.5f, -0.5f);
+			glVertex3f(0.5f, 0.5f, 0.5f);
+			glVertex3f(0.5f, -0.5f, 0.5f);
+			glVertex3f(0.5f, -0.5f, -0.5f);
 		glEnd();  // End of drawing color-cube
 	glPopMatrix();
 }

1ee3d8a8052518d5441b105413897bfd9f5f79b0
Robert Chisholm <robadob@robadob.org>
2016-10-26 22:59:17 +0100

    Setup binder for single colour attachment.
    Need to modify logic to permit multiple colour attachments.
    
    Then sort out depth/stencil attachments, need to consider depth_stencil attach pt.

--- sdl_exp/visualisation/multipass/FrameBuffer.cpp
+++ sdl_exp/visualisation/multipass/FrameBuffer.cpp
@@ -1,21 +1,94 @@
 #include "FrameBuffer.h"
 
-FrameBuffer::FrameBuffer()
+FrameBuffer::FrameBuffer(Color color, Depth depth, Stencil stencil, float scale, bool doClear, glm::vec3 clearColor)
+	: FrameBuffer(color, depth, stencil, scale, glm::uvec2(0), clearColor, doClear)
+{ }
+FrameBuffer::FrameBuffer(Color color, Depth depth, Stencil stencil, glm::uvec2 dimensions, bool doClear, glm::vec3 clearColor)
+	: FrameBuffer(color, depth, stencil, 0, dimensions, clearColor, doClear)
+{ }
+FrameBuffer::FrameBuffer(Color color, Depth depth, Stencil stencil, float scale, glm::uvec2 dimensions, glm::vec3 clearColor, bool doClear)
+	: scale(scale)
+	, dimensions(dimensions)
+	, clearColor(clearColor)
+	, doClear(doClear)
+	, colorConf(color)
+	, depthConf(depth)
+	, stencilConf(stencil)
+	, colorName(0)
 {
-	glGenFramebuffers(1, &name);
+	GL_CALL(glGenFramebuffers(1, &name));
+	makeColor();
+	makeDepth();
+	makeStencil();
 }
 FrameBuffer::~FrameBuffer()
 {
+	if (colorConf.type == Texture)
+		GL_CALL(glDeleteTextures(1, &colorName));
+	if (colorConf.type == RenderBuffer)
+		GL_CALL(glDeleteRenderbuffers(1, &colorName));
 	glDeleteFramebuffers(1, &name);
 }
-
-bool FrameBuffer::isValid()
+void FrameBuffer::makeColor()
 {
-	GLuint prevFBO = 0;
+	if (colorConf.type == Texture)
+	{
+		if (colorConf.texName == 0)
+		{
+			//If it doesn't exist
+			if (colorName == 0)
+				GL_CALL(glGenTextures(1, &colorName));
+
+			GL_CALL(glBindTexture(GL_TEXTURE_2D, colorName));
+
+			//Size the texture
+			GL_CALL(glTexImage2D(GL_TEXTURE_2D, 0, colorConf.colorInternalFormat, dimensions.x, dimensions.y, 0, colorConf.colorFormat, colorConf.colorType, nullptr));
+
+			//Config for mipmap access
+			GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR));
+			GL_CALL(glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR));
+
+			GL_CALL(glBindTexture(GL_TEXTURE_2D, 0));
+		}
+		else
+			colorName = colorConf.texName;
+		//Bind the tex to our framebuffer
+		GLuint prevFBO = getActiveFB();
+		GL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, name));
+
+		GL_CALL(glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + colorConf.attachmentId, GL_TEXTURE_2D, colorName, 0));
+
+		GL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, prevFBO));
+	}
+	else if (colorConf.type == RenderBuffer)
+	{
+		//If it doesn't exist, make
+		if (colorName == 0)
+			GL_CALL(glGenRenderbuffers(1, &colorName));
+		//Set storage
+		GL_CALL(glBindRenderbuffer(GL_RENDERBUFFER, colorName));
+		GL_CALL(glRenderbufferStorage(GL_RENDERBUFFER, colorConf.colorInternalFormat, dimensions.x, dimensions.y));
+		GL_CALL(glBindRenderbuffer(GL_RENDERBUFFER, 0));
+
+		//Bind to our framebuffer
+		GLuint prevFBO = getActiveFB();
+		GL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, name));
+		GL_CALL(glFramebufferRenderbuffer(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0 + colorConf.attachmentId, GL_RENDERBUFFER, colorName));
+		GL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, prevFBO));
+	}
+
+}
+GLuint FrameBuffer::getActiveFB()
+{
+	GLuint prevFB = 0;
 	// GL_FRAMEBUFFER_BINDING Enum has MANY names based on extension/version
 	// but they all map to 0x8CA6
-	GL_CALL(glGetIntegerv(0x8CA6, (GLint*)&prevFBO));
-
+	GL_CALL(glGetIntegerv(0x8CA6, (GLint*)&prevFB));
+	return prevFB;
+}
+bool FrameBuffer::isValid()
+{
+	GLuint prevFBO = getActiveFB();
 	GL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, name));
 	GLint st = GL_CALL(glCheckFramebufferStatus(GL_FRAMEBUFFER));
 	GL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, prevFBO));
@@ -26,7 +99,9 @@ void FrameBuffer::resize(int width, int height)
 	if (scale>0)
 	{
 		dimensions = glm::ivec2(ceil(width*scale), ceil(height*scale));
-		//todo: resize textures
+		makeColor();
+		makeDepth();
+		makeStencil();
 	}
 }
 bool FrameBuffer::use()
@@ -41,7 +116,7 @@ bool FrameBuffer::use()
 	{
 		if (doClear)
 		{
-			GL_CALL(glClearColor(clearColor.x, clearColor.y, clearColor.z, clearColor.w));
+			GL_CALL(glClearColor(clearColor.x, clearColor.y, clearColor.z, 1));
 			GL_CALL(glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT));
 		}
 		GL_CALL(glViewport(0, 0, dimensions.x, dimensions.y));
@@ -50,4 +125,9 @@ bool FrameBuffer::use()
 	//Rebind prev buffer on failure
 	GL_CALL(glBindFramebuffer(GL_FRAMEBUFFER, prevFBO));
 	return false;
+}
+
+void FrameBuffer::setColorBuffer()
+{
+	
 }
--- sdl_exp/visualisation/multipass/FrameBuffer.h
+++ sdl_exp/visualisation/multipass/FrameBuffer.h
@@ -9,7 +9,135 @@
 class FrameBuffer
 {
 public:
-	FrameBuffer();
+	enum AttatchType {Disabled, Texture, RenderBuffer};
+	struct Color
+	{
+		/**
+		 * How to provide the attatchement
+		 * @note You probably want Disabled or Texture, You can't sample from a renderbuffer
+		 */
+		AttatchType type;
+		/**
+		 * The internal format of the texture
+		 * @note Required for both texture and renderbuffer types
+		 * @see glTexImage2D() and glRenderbufferStorage()
+		 */
+		GLint colorInternalFormat;//GL_RGB, GL_RGBA8
+		/**
+		 * The color format of the texture
+		 * @note Only required for texture types
+		 * @see glTexImage2D() 
+		 */
+		GLenum colorFormat;//GL_RGB, GL_BGRA
+		/**
+		 * The storage type of the texture
+		 * @note Only required for texture types
+		 * @see glTexImage2D() 
+		 */
+		GLenum colorType;//GL_UNSIGNED_BYTE
+		/**
+		 * Allows multiple colour attatchments to a FBO.
+		 * The limit is provided by GL_MAX_COLOR_ATTACHMENTS
+		 * @note Valid values range from 0 to GL_MAX_COLOR_ATTACHMENTS - 1 and should be unique
+		 */
+		unsigned int attachmentId;
+		/**
+		 * Number of samples required
+		 * If less than or equal to 1, multisample is disabled
+		 * @note Only required for multisample renderbuffer types
+		 * @see glRenderbufferStorageMultisample()
+		 */
+		GLsizei samples;
+		/**
+		 * The name of the unmanaged texture to use
+		 * @note Only set this value to something other than 0 if you want to use an unmanaged texture
+		 * @see This is the value which glGenTextures() creates
+		 */
+		GLuint texName;
+	};
+	struct Depth
+	{
+		/**
+		* How to provide the attatchement
+		* @note You probably want Disabled or Texture, You can't sample from a renderbuffer
+		*/
+		AttatchType type;
+		/**
+		* The internal format of the texture
+		* @note Required for both texture and renderbuffer types
+		* @see glTexImage2D() and glRenderbufferStorage()
+		*/
+		GLint colorInternalFormat;//GL_DEPTH_COMPONENT16, GL_DEPTH_COMPONENT24, GL_DEPTH_COMPONENT32, GL_DEPTH_COMPONENT32F
+		/**
+		* The color format of the texture
+		* @note Only required for texture types
+		* @note No variation permitted for depth textures
+		* @see glTexImage2D()
+		*/
+		const GLenum colorFormat = GL_DEPTH_COMPONENT;
+		/**
+		* The storage type of the texture
+		* @note Only required for texture types
+		* @see glTexImage2D()
+		*/
+		const GLenum colorType = GL_UNSIGNED_BYTE;
+		/**
+		* Number of samples required
+		* If less than or equal to 1, multisample is disabled
+		* @note Only required for multisample renderbuffer types
+		* @see glRenderbufferStorageMultisample()
+		*/
+		GLsizei samples;
+		/**
+		* The name of the unmanaged texture to use
+		* @note Only set this value to something other than 0 if you want to use an unmanaged texture
+		* @see This is the value which glGenTextures() creates
+		*/
+		GLuint texName;
+	};
+	struct Stencil
+	{
+		/**
+		* How to provide the attatchement
+		* @note You probably want Disabled or Texture, You can't sample from a renderbuffer
+		*/
+		AttatchType type;
+		/**
+		* The internal format of the texture
+		* @note Required for both texture and renderbuffer types
+		* @see glTexImage2D() and glRenderbufferStorage()
+		*/
+		GLint colorInternalFormat;//GL_DEPTH24_STENCIL8, GL_DEPTH32F_STENCIL8, GL_STENCIL_INDEX8
+		/**
+		* The color format of the texture
+		* @note Only required for texture types
+		* @note No variation permitted for depth textures
+		* @see glTexImage2D()
+		*/
+		//GL_DEPTH_STENCIL (for the two depth_stencil internal formats)
+		//GL_STENCIL_INDEX8 (for the GL_STENCIL_INDEX8 internal format, may not be supported prior to gl 4.3)
+		/**
+		* The storage type of the texture
+		* @note Only required for texture types
+		* @see glTexImage2D()
+		*/
+		const GLenum colorType = GL_UNSIGNED_BYTE;
+		/**
+		* Number of samples required
+		* If less than or equal to 1, multisample is disabled
+		* @note Only required for multisample renderbuffer types
+		* @see glRenderbufferStorageMultisample()
+		*/
+		GLsizei samples;
+		/**
+		* The name of the unmanaged texture to use
+		* @note Only set this value to something other than 0 if you want to use an unmanaged texture
+		* @see This is the value which glGenTextures() creates
+		*/
+		GLuint texName;
+	};
+	FrameBuffer(Color color, Depth depth, Stencil stencil, float scale=1.0f, bool doClear = true, glm::vec3 clearColor = glm::vec3(0));
+	FrameBuffer(Color color, Depth depth, Stencil stencil, glm::uvec2 dimensions, bool doClear = true, glm::vec3 clearColor = glm::vec3(0));
 	~FrameBuffer();
 	bool isValid();
     GLuint getName() const{ return name; }
@@ -23,11 +151,22 @@ public:
 	void resize(int width, int height);
 	bool use();
 private:
-	float scale;
-	glm::ivec2 dimensions;
+	FrameBuffer(Color color, Depth depth, Stencil stencil, float scale, glm::uvec2 dimensions, glm::vec3 clearColor, bool doClear);
+	void makeColor();
+	void makeDepth();
+	void makeStencil();
+	static GLuint getActiveFB();
+	
+	const float scale;
+	glm::uvec2 dimensions;
     GLuint name;
-	glm::vec4 clearColor;
+	glm::vec3 clearColor;
 	bool doClear;
+
+	const Color colorConf;
+	GLuint colorName;
+	const Depth depthConf;
+	const Stencil stencilConf;
 };
 
 #endif

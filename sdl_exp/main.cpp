0d21cc6fc68747594bb005584b4648f8a96b82ae
Robert Chisholm <R.Chisholm@sheffield.ac.uk>
2016-12-24 15:13:10 +0000

    Adjusted main to permit launching of either scene.

--- sdl_exp/main.cpp
+++ sdl_exp/main.cpp
@@ -2,10 +2,26 @@
 #include "TwoPassScene.h"
 #include "visualisation/multipass/FrameBufferAttachment.h"
 
-int main()
+int main(int count, char **args)
 {
+    int sceneId = 0;
+    if (count > 1)
+        sceneId = atoi(args[1]);
     Visualisation v = Visualisation("Visulisation Example", 1280, 720);
-	v.setScene(std::make_unique<TwoPassScene>(v));
+    switch (sceneId)
+    {
+        case 0:
+            {
+                v.setScene(std::make_unique<EntityScene>(v));
+            }
+            break;
+        case 1:
+        default:
+            {
+               v.setScene(std::make_unique<TwoPassScene>(v));
+            }
+            break;
+    }
     v.run();
 
     return 0;

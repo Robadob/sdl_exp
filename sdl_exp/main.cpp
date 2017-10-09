03c1b3b19dd81eef5f9632517313159914c74034
Robert Chisholm <robadob@robadob.org>
2017-10-09 23:17:11 +0100

    Adjusted main.cpp to test VR mode.

--- sdl_exp/main.cpp
+++ sdl_exp/main.cpp
@@ -1,27 +1,29 @@
 #include "EntityScene.h"
 #include "TwoPassScene.h"
-#include "visualisation/multipass/FrameBufferAttachment.h"
+#include "visualisation/VisualisationVR.h"
 
 int main(int count, char **args)
 {
-    int sceneId = 0;
-    if (count > 1)
-        sceneId = atoi(args[1]);
-    Visualisation v = Visualisation("Visulisation Example", 1280, 720);
-    switch (sceneId)
-    {
-        case 0:
-            {
-                v.setScene(std::make_unique<EntityScene>(v));
-            }
-            break;
-        case 1:
-        default:
-            {
-               v.setScene(std::make_unique<TwoPassScene>(v));
-            }
-            break;
-    }
+	VisualisationVR v = VisualisationVR("Visulisation Example", 1280, 720);
+	v.setScene(std::make_unique<EntityScene>(v));
+    //int sceneId = 0;
+    //if (count > 1)
+    //    sceneId = atoi(args[1]);
+    //Visualisation v = Visualisation("Visulisation Example", 1280, 720);
+    //switch (sceneId)
+    //{
+    //    case 0:
+    //        {
+    //            v.setScene(std::make_unique<EntityScene>(v));
+    //        }
+    //        break;
+    //    case 1:
+    //    default:
+    //        {
+    //           v.setScene(std::make_unique<TwoPassScene>(v));
+    //        }
+    //        break;
+    //}
     v.run();
 
     return 0;

711b80b06b1f198d4012f5ffd3c8861cb7f2f7cd
Robert Chisholm <r.chisholm@sheffield.ac.uk>
2017-10-12 16:26:18 +0100

    Started work on renderable scene graph.
    Need to pull in Assimp shaders.

--- sdl_exp/visualisation/interface/Renderable.h
+++ sdl_exp/visualisation/interface/Renderable.h
@@ -7,6 +7,7 @@
 
 #include "Viewport.h"
 #include "Camera.h"
+#include <vector>
 
 /**
  * Represents things which hold shaders (and can be rendered)
@@ -54,6 +55,77 @@ public:
 	{
 		setProjectionMatPtr(visualisation->getProjMatPtr());
 	}
+    //Render
+    virtual void render(const unsigned int &shaderIndex=0, const glm::mat4 &transform = glm::mat4()) = 0;
+    virtual void renderSceneGraph(const unsigned int &shaderIndex = 0, const glm::mat4 &transform = glm::mat4())
+    {
+        //Render me with transform
+        render(shaderIndex, transform);
+        //Render children with transform
+        for (const auto &pair : children)
+        {
+            if (auto child = std::get<0>(pair).lock())
+            {
+                child->render(std::get<2>(pair), (*std::get<1>(pair)) * transform);
+            }
+        }
+    }
+    //Scene graph mgmt
+    void addChild(std::weak_ptr<Renderable> a, std::shared_ptr<glm::mat4> b = std::make_shared<glm::mat4>(), unsigned int shaderIndex = 0)
+    {
+        children.push_back(std::make_tuple( a, b, shaderIndex));
+    };
+    void clearChildren()
+    {
+        children.clear();
+    }
+    unsigned int removeChildren(const std::shared_ptr<Renderable> &a)
+    {
+        unsigned int rtn = 0;
+        for (auto &&it = children.begin(); it != children.end();)
+        {
+            if (std::get<0>(*it).lock())
+            {
+                it = children.erase(it);
+                rtn++;
+                continue;
+            }
+            ++it;
+        }
+        return rtn;
+    }
+    unsigned int removeChildren(const std::shared_ptr<glm::mat4> &b)
+    {
+        unsigned int rtn = 0;
+        for (auto &&it = children.begin(); it != children.end();)
+        {
+            if (std::get<1>(*it) == b)
+            {
+                it = children.erase(it);
+                rtn++;
+                continue;
+            }
+            ++it;
+        }
+        return rtn;
+    }
+    unsigned int removeChildren(const std::shared_ptr<Renderable> &a, const std::shared_ptr<glm::mat4> &b, const unsigned int shaderIndex = 0)
+    {
+        unsigned int rtn = 0;
+        for (auto &&it = children.begin(); it != children.end();)
+        {
+            if (std::get<0>(*it).lock() == a&&std::get<1>(*it) == b)
+            {
+                it = children.erase(it);
+                rtn++;
+                continue;
+            }
+            ++it;
+        }
+        return rtn;
+    }
+private:
+    std::list<std::tuple<std::weak_ptr<Renderable>, std::shared_ptr<glm::mat4>, unsigned int>> children;
 
 };
 

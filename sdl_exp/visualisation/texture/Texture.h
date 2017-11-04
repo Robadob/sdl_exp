b88dd85911130bcfa46753a0eeb0529906f7ccdc
Robert Chisholm <robadob@robadob.org>
2017-11-04 09:17:45 +0000

    Added a bunch of comments.

--- sdl_exp/visualisation/texture/Texture.h
+++ sdl_exp/visualisation/texture/Texture.h
@@ -14,6 +14,11 @@
 class Texture
 {
 public:
+	/**
+	 * This structure holds the necessary Enums for calling various OpenGL texture funtions
+	 * format, internal format and type, see docs of glTexImage2D() or similar for explanation
+	 * pixelSize is the size of a single pixel in bytes, this is included for convenience (and accuracy)
+	 */
 	struct Format
 	{
 		Format(GLenum format, GLenum internalFormat, size_t pixelSize, GLenum type)
@@ -31,47 +36,104 @@ public:
 		const GLenum type;
 		const size_t pixelSize;//inBytes
 	};
+	/**
+	 * Options
+	 */
+	//Toggles anistropic filtering, this is good at steep angles
 	static const unsigned long long DISABLE_ANISTROPIC_FILTERING;
-
+	//Selects the minifying filter, used when the texture is being downscaled
 	static const unsigned long long FILTER_MIN_NEAREST;
 	static const unsigned long long FILTER_MIN_LINEAR;
 	static const unsigned long long FILTER_MIN_NEAREST_MIPMAP_NEAREST;
 	static const unsigned long long FILTER_MIN_LINEAR_MIPMAP_NEAREST;
 	static const unsigned long long FILTER_MIN_NEAREST_MIPMAP_LINEAR;
 	static const unsigned long long FILTER_MIN_LINEAR_MIPMAP_LINEAR;
-
+	//Selects the magnifying filter, used when the texture is being upscaled
 	static const unsigned long long FILTER_MAG_LINEAR;
 	static const unsigned long long FILTER_MAG_NEAREST;
-
+	//Selects how texture coordinates are wrapped
 	static const unsigned long long WRAP_REPEAT;
 	static const unsigned long long WRAP_CLAMP_TO_EDGE;
 	static const unsigned long long WRAP_CLAMP_TO_BORDER;
 	static const unsigned long long WRAP_MIRRORED_REPEAT;
 	static const unsigned long long WRAP_MIRROR_CLAMP_TO_EDGE;
-
+	//Toggle's the use of mip maps (4 levels are used with mipmaps, this is not currently changeable)
 	static const unsigned long long DISABLE_MIPMAP;
+	/**
+	 * @return The GLenum representing the type of texture, e.g. GL_TEXTURE_2D, GL_TEXTURE_CUBEMAP, GL_TEXTURE_BUFFER
+	 */
 	GLenum getType() const { return type; }
+	/**
+	 * @return The unique name given to the texture by OpenGL (allocated by glGenTextures())
+	 */
 	GLenum getName() const { return glName; }
+	/**
+	 * @return The texture unit we have assigned to this texture
+	 * @note Texture's of different type's may share this texture unit, using the appropriate sampler in shaders will select the correct texture
+	 * @note Currently we are not set up to have more textures of any type than the number of unique units (~92)
+	 */
 	GLenum getTextureUnit() const { return textureUnit; }
+	/**
+	 * @return The name assigned to the texture
+	 * @note For textures loaded from file, this will be the file path
+	 * @note For raw textures this will simply be the string of the enum returned by getType()
+	 */
 	std::string getReference() const { return reference; }
-
+	/**
+	 * @return Whether the texture is the currently bound texture of it's given type to the assigned texture unit
+	 */
 	virtual bool isBound() const = 0;
+	/**
+	 * Binds the texture to it's currently assigned texture unit
+	 */
 	void bind() const;
-
+	/**
+	 * Returns whether the anistropic filtering option is enabled according to the selected options
+	 */
 	bool enableAnistropicOption() const;
+	/**
+	 * Returns the GLenum of the selected minifying filter option
+	 */
 	GLenum filterMinOption() const;
+	/**
+	 * Returns the GLenum of the selected magnifying filter option
+	 */
 	GLenum filterMagOption() const;
+	/**
+	 * Returns the GLenum of the selected texture wrap option
+	 */
 	GLenum wrapOption() const;
+	/**
+	 * Returns whether mipmapping is enabled according to the selected options
+	 */
 	bool enableMipMapOption() const;
+	/**
+	 * @param fileExtension A given string of a file extension
+	 * @return Wether the image loader is capable of handling the given extension
+	 * @note Indifferent to case-sensitivity and the inclusion of initial '.'
+	 */
 	static bool supportsExtension(const std::string &fileExtension);
+	/**
+	 * Virtual destructor, deallocate the OpenGL texture
+	 * @note Does not deallocate external texture's
+	 */
 	virtual ~Texture();
 protected:
-	Texture(const Texture& b) = delete;//Subclasses should call the normal constructor
+	/**
+	 * Remove all copy/move/assignment contructors
+	 * Subclasses should call the regular constructor from any copy constructors.
+	 */
+	Texture(const Texture& b) = delete;
 	Texture(const Texture&& b) = delete;
 	Texture& operator= (const Texture& b) = delete;
 	Texture& operator= (const Texture&& b) = delete;
 	/**
 	 * @param type The texture type e.g. GL_TEXTURE_2D
+	 * @param textureUnit The chosen texture unit to be used by the texture
+	 * @param format The internal format specifiers of the texture
+	 * @param reference A string to identifyy the texture
+	 * @param options The filtering/wrapping/mipmap options for how the texture should be handled
+	 * @param glName If non 0 this will force the texture to use an external GL texture, rather than allocate it's own
 	 */
 	Texture(GLenum type, GLuint textureUnit, const Format &format, const std::string &reference, unsigned long long options = FILTER_MIN_LINEAR_MIPMAP_LINEAR | FILTER_MAG_LINEAR | WRAP_REPEAT, GLuint glName=0);
 	void applyOptions();

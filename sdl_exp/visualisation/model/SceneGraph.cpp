1d5d9247648b90552dac79f275b87d6e859ce70c
Robert Chisholm <robadob@robadob.org>
2017-11-08 22:00:09 +0000

    Added, grab/drop mechanics. and finished children add/remove.

--- sdl_exp/visualisation/model/SceneGraph.cpp
+++ sdl_exp/visualisation/model/SceneGraph.cpp
@@ -1,12 +1,17 @@
 #include "SceneGraph.h"
 
-SceneGraphEdge::SceneGraphEdge(const std::shared_ptr<SceneGraphVertex> &vertex, const glm::mat4 &localTransform)
+SceneGraphEdge::SceneGraphEdge(const std::shared_ptr<SceneGraphVertex> &vertex, const glm::mat4 &attachmentTransform)
     : vertex(vertex)
-    , localTransform(localTransform)
+	, localTransform(attachmentTransform)
     , computedGlobalTransform(1)
     , hasChanged(true)//Set changed as we lack parent transforms
-{  }
-    
+{
+	vertex->incrementAttachments();
+}
+SceneGraphEdge::~SceneGraphEdge()
+{
+	vertex->decrementAttachments();
+}
 inline void SceneGraphEdge::propagateUpdate(const glm::mat4 &parentTransform)
 {
     //Update my transform
@@ -17,6 +22,27 @@ inline void SceneGraphEdge::propagateUpdate(const glm::mat4 &parentTransform)
     vertex->propagateUpdate(computedGlobalTransform);
 }
 
+SceneGraphEdge& SceneGraphEdge::grab(const std::shared_ptr<SceneGraphVertex> &target)
+{
+	assert(target->attachmentCount() == 0);//If this is attached elsewhere when released it will have a bad effect as model mat chagnes
+	return vertex->addChild(target, inverse(computedGlobalTransform));
+}
+bool SceneGraphEdge::release(const std::shared_ptr<SceneGraphVertex> &target)
+{
+	const SceneGraphEdge *edge = vertex->getChildEdge(target);
+	if (edge)
+	{
+		assert(target->attachmentCount() == 1);//Changing model matrix will do bad stuff if this is attached elsewhere
+		target->setModelMat(computedGlobalTransform*edge->localTransform*target->getModelMat());
+		vertex->removeChild(*edge);
+		return true;
+	}
+	return false;
+}
+bool SceneGraphEdge::release(SceneGraphEdge& target)
+{
+	return release(target.vertex);
+}
 void SceneGraphVertex::renderSceneGraph(const glm::mat4 &rootTransform, const glm::mat4 &sceneTransform)
 {
     //Render this
@@ -31,10 +57,46 @@ void SceneGraphVertex::renderSceneGraph(const glm::mat4 &rootTransform, const gl
         edge.renderSceneGraph(rootTransform);
     }
 }
+SceneGraphEdge &SceneGraphVertex::addChild(const std::shared_ptr<SceneGraphVertex> &child, const glm::mat4 &attachmentTransform = glm::mat4(1))
+{
+	children.emplace_back(SceneGraphEdge(child, attachmentTransform));
+	return *children.end();//C++17 allows to return emplace_back
+}
+SceneGraphVertex::SceneGraphVertex()
+	:attachments(0)
+{
+
+}
+/**
+ * Unsure if this will work, can we delete a reference?
+ * Can we get the pointer to an iterator and compare with pointer to reference?
+ */
+void SceneGraphVertex::removeChild(const SceneGraphEdge &edge)
+{
+	for (auto &&it = children.begin(); it != children.end(); ++it)
+	{
+		if (&(*it) == &edge)
+		{
+			children.erase(it);
+			return;
+		}
+	}
+}
 void SceneGraphVertex::propagateUpdate(const glm::mat4 &parentTransform)
 {
     for (SceneGraphEdge &edge : children)
     {
         edge.propagateUpdate(parentTransform);
     }
+}
+const SceneGraphEdge *SceneGraphVertex::getChildEdge(const std::shared_ptr<SceneGraphVertex> &child)
+{
+	for (auto &&it = children.begin(); it != children.end(); ++it)
+	{
+		if (it->vertex == child)
+		{
+			return &(*it);
+		}
+	}
+	return nullptr;
 }
--- sdl_exp/visualisation/model/SceneGraph.h
+++ sdl_exp/visualisation/model/SceneGraph.h
@@ -12,13 +12,34 @@ class SceneGraphEdge;
  */
 class SceneGraphVertex
 {
-    friend class SceneGraphEdge; //Accesses private renderSceneGraph
+    friend class SceneGraphEdge; //Accesses private renderSceneGraph(const glm::mat4 &, const glm::mat4 &)
     typedef SceneGraphVertex SGVertex;
 public:
+	SceneGraphVertex();
+	/**
+	 * Copy constructor
+	 * @TODO
+	 */
+	SceneGraphVertex(const SceneGraphVertex& b) = delete;
+	/**
+	 * Move constructor
+	 * @TODO?
+	 */
+	SceneGraphVertex(const SceneGraphVertex&& b) = delete;
+	/**
+	 * Copy assignment operator
+	 * @TODO?
+	 */
+	SceneGraphVertex& operator= (const SceneGraphVertex& b) = delete;
+	/**
+	 * Move assignment operator
+	 * @TODO?
+	 */
+	SceneGraphVertex& operator= (const SceneGraphVertex&& b) = delete;
     virtual ~SceneGraphVertex() = default;
     virtual glm::mat4 getModelMat() const=0;
     virtual const glm::mat4 *getModelMatPtr() const=0;
-    //virtual void setModelMat(const glm::mat4 *newModelMat)=0;
+    virtual void setModelMat(const glm::mat4 &newModelMat)=0;
     /**
      * Renders the current object applying transform before the internal model matrix
      * @param transform Transformation matrix to be applied before the model matrix
@@ -34,13 +55,35 @@ public:
     {
         renderSceneGraph(transform, glm::mat4(1));
     }
+	/**
+	 * Attaches a SceneGraphVertex to this models sub-graph with the given attachment transform
+	 */
+	SceneGraphEdge &addChild(const std::shared_ptr<SceneGraphVertex> &child, const glm::mat4 &attachmentTransform = glm::mat4(1));
+	/**
+	 * Removes the specified edge from this model's sub graph
+	 */
+	void removeChild(const SceneGraphEdge &edge);
+	/**
+	 * Returns a pointer to the first direct child of this model which shares the given pointer, else nullptr
+	 */
+	const SceneGraphEdge *getChildEdge(const std::shared_ptr<SceneGraphVertex> &child);
     /**
      * Forwards propogation to each child
      * @param parentTransform Transform to be applied before local transform
      */
     void propagateUpdate(const glm::mat4 &parentTransform);
+	/**
+	 * Tracks the number of SceneGraphEdge's this SceneGraphVertex is contained within
+	 */
+	inline unsigned int attachmentCount() const { return attachments; }
 private:
     void renderSceneGraph(const glm::mat4 &rootTransform, const glm::mat4 &sceneTransform);
+	void incrementAttachments() { attachments++; };
+	/**
+	 * Safety check to ensure attachments doesn't loop
+	 */
+	void decrementAttachments() { if (attachments)attachments--; };
+	unsigned int attachments;
     std::list<SceneGraphEdge> children;
 };
 
@@ -51,49 +94,73 @@ private:
  */
 class SceneGraphEdge
 {
+	friend class SceneGraphVertex; //Accesses private constructor
     typedef SceneGraphVertex SGEdge;
-public:
+private:
     /**
-     *
+     * Constructor, only to be called by SceneGraphVertex
      */
-    SceneGraphEdge(const std::shared_ptr<SceneGraphVertex> &vertex, const glm::mat4 &localTransform = glm::mat4(1));
+	SceneGraphEdge(const std::shared_ptr<SceneGraphVertex> &vertex, const glm::mat4 &attachmentTransform);
+	~SceneGraphEdge();
+	/**
+	 * Copy constructor
+	 * @TODO
+	 */
+	SceneGraphEdge(const SceneGraphEdge& b) = delete;
+	/**
+	 * Move constructor
+	 * @TODO?
+	 */
+	SceneGraphEdge(const SceneGraphEdge&& b) = delete;
+	/**
+	 * Copy assignment operator
+	 * @TODO?
+	 */
+	SceneGraphEdge& operator= (const SceneGraphEdge& b) = delete;
+	/**
+	 * Move assignment operator
+	 * @TODO?
+	 */
+	SceneGraphEdge& operator= (const SceneGraphEdge&& b) = delete;
+public:
     /////////////////////////
     // Matrix Manipulation //
     /////////////////////////
     /**
      * Resets the transformation matrix to the identity matrix
      */
-    inline void resetTransform() { localTransform = glm::mat4(1); hasChanged = true; }
+	inline SceneGraphEdge& resetTransform() { localTransform = glm::mat4(1); hasChanged = true; return *this; }
     /**
      * Replaces the transformation matrix with the provided one
      * @param newTransform The new transform matrix to be used
      */
-    inline void setTransform(glm::mat4 newTransform) { localTransform = newTransform; hasChanged = true; }
+	inline SceneGraphEdge& setTransform(glm::mat4 newTransform) { localTransform = newTransform; hasChanged = true; return *this; }
     /**
      * Scales the existing transformation matrix
      * @param scale The 3-dimensional scale factor to be used
      */
-    inline void scale(glm::vec3 scale) { localTransform *= glm::scale(scale); hasChanged = true; }
+	inline SceneGraphEdge& scale(glm::vec3 scale) { localTransform *= glm::scale(scale); hasChanged = true; return *this; }
     /**
      * Rotates the existing transformation matrix about an axis
      * @param axis The axis to rotate about
      * @param angleDegrees The number of degrees to rotate
      */
-    inline void rotate(glm::vec3 axis, float angleDegrees) { localTransform *= glm::rotate(angleDegrees, axis); hasChanged = true; }
+	inline SceneGraphEdge& rotate(glm::vec3 axis, float angleDegrees) { localTransform *= glm::rotate(angleDegrees, axis); hasChanged = true; return *this; }
     /**
      * Translates the existing transformation matrix
      * @param translation The translation to use
      */
-    inline void translate(glm::vec3 translation){ localTransform *= glm::translate(translation); hasChanged = true; }
+	inline SceneGraphEdge& translate(glm::vec3 translation){ localTransform *= glm::translate(translation); hasChanged = true; return *this; }
     /**
      * Replaces the absolute translation of the transformation matrix
      * @param offset The new absolute translation
      */
-    inline void setOffset(glm::vec3 offset) {
+	inline SceneGraphEdge& setOffset(glm::vec3 offset) {
         localTransform[3][0] = offset.x;
         localTransform[3][1] = offset.y;
         localTransform[3][2] = offset.z;
-        hasChanged = true;
+		hasChanged = true; 
+		return *this;
     };
     /////////////////////////////////////
     // Convenience Matrix Manipulation //
@@ -102,29 +169,44 @@ public:
      * Uniformly scales the existing transformation matrix
      * @param scale The scale factor to be used
      */
-    inline void scale(float scale) { this->scale(glm::vec3(scale)); }
+	inline SceneGraphEdge& scale(float scale) { return this->scale(glm::vec3(scale)); }
     /**
      * @see scale(glm::vec3)
      */
-    inline void scale(float x, float y, float z) { this->scale(glm::vec3(x, y, z)); }
+	inline SceneGraphEdge& scale(float x, float y, float z) { return this->scale(glm::vec3(x, y, z)); }
     /**
      * @see rotate(glm::vec3, float)
      */
-    inline void rotate(float axisX, float axisY, float axisZ, float angleDegrees) { this->rotate(glm::vec3(axisX, axisY, axisZ), angleDegrees); }
+	inline SceneGraphEdge& rotate(float axisX, float axisY, float axisZ, float angleDegrees) { return this->rotate(glm::vec3(axisX, axisY, axisZ), angleDegrees); }
     /**
      * @see translate(glm::vec3)
      */
-    inline void translate(float x, float y, float z){ this->translate(glm::vec3(x, y, z)); }
+	inline SceneGraphEdge& translate(float x, float y, float z){ return this->translate(glm::vec3(x, y, z)); }
     /**
      * @see setOffset(glm::vec3)
      */
-    inline void setOffset(float x, float y, float z){ this->setOffset(glm::vec3(x, y, z)); }
+	inline SceneGraphEdge& setOffset(float x, float y, float z){ return this->setOffset(glm::vec3(x, y, z)); }
     ///////////////
     // Accessors //
     ///////////////
-    inline bool expired() const { return hasChanged; }
     inline glm::mat4 getTransform() const { return computedGlobalTransform; }
-    inline const glm::mat4 *getTransformPtr() const { return &computedGlobalTransform; }
+	inline const glm::mat4 *getTransformPtr() const { return &computedGlobalTransform; }
+	////////////////////////////////////
+	// Advanced Scene Graph Mechanics //
+	////////////////////////////////////
+	/**
+	 * Attaches the object in it's current offset from the edge to the scene graph
+	 * @note On release this will affect the SceneGraphVertex's model matrix
+	 * therefore the SceneGraphVertex should not have any existing attachments
+	 */
+	SceneGraphEdge& grab(const std::shared_ptr<SceneGraphVertex> &target);
+	/**
+	 * Drops the specified object, updating it's model matrix so that it remains in the same position it were at release
+	 */
+	bool release(const std::shared_ptr<SceneGraphVertex> &target);
+	bool release(SceneGraphEdge& target);
+private:
+	inline bool expired() const { return hasChanged; }
     /**
      * Updates computedGlobalTransform in this and all child edges
      * @param parentTransform Applies this matrix before localTransform
@@ -140,8 +222,7 @@ public:
         //Render hierarchy
         vertex->renderSceneGraph(rootTransform, computedGlobalTransform);
     }
-private:
-    std::shared_ptr<SceneGraphVertex> vertex;
+    const std::shared_ptr<SceneGraphVertex> vertex;
     glm::mat4 localTransform;
     glm::mat4 computedGlobalTransform;
     bool hasChanged;
